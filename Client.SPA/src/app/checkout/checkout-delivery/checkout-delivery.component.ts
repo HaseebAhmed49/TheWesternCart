0a76c4d87408d17243634cb2d586e0a56dfab462
haseebahmed <haseebahmed02@gmail.com>
2025-02-26 22:35:08 +0000

    Added Blank CheckOut Components

---
+++ Client.SPA/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-delivery',
+  imports: [],
+  templateUrl: './checkout-delivery.component.html',
+  styleUrl: './checkout-delivery.component.sass'
+})
+export class CheckoutDeliveryComponent {
+
+}
